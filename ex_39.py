9b0bd09f471be6c000d1805527427e594a6e20a5
GorbachevaJuliana <gorbacheva.juliana@gmail.com>
2024-10-01 20:43:53 +0300

    Add files via upload

---
+++ ex_39.py
@@ -0,0 +1,10 @@
+month = input()
+res = 0
+if month == 'Январь' or month == 'Март' or month == 'Май' or month == 'Июль' or month == 'Август' or month == 'Октябрь' or month == 'Декабрь':
+    res = 31
+elif month == 'Апрель' or month == 'Июнь' or month == 'Сентябрь' or month == 'Ноябрь':
+    res = 30
+else:
+    res = '28 или 29'
+print(f'{month} = {res}')
+
