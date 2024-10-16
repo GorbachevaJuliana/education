e926bb2aa52b081c2e94e441c86b8c0771056d6f
Юлиана <gorbacheva.juliana@gmail.com>
2024-10-16 18:59:57 +0300

    Add files via upload

---
+++ ex_42.py
@@ -0,0 +1,19 @@
+n = input()
+res = 0
+if n == 'C4':
+    res = 261,63
+    print(type(res))
+elif n == 'D4':
+    res = 293,66
+elif n == 'E4':
+    res = 329,63
+elif n == 'F4':
+    res = 349,23
+elif n == 'G4':
+    res = 392,00
+elif n == 'A4':
+    res = 440,00
+elif n == 'B4':
+    res = 493,88
+print(f'Частота ноты {n} равна {res}')
+# У меня не получается сделать переменную res int()
