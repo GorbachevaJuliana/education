6d224ea0f75043fa60de174be48f4b7ffe621c88
Юлиана <gorbacheva.juliana@gmail.com>
2024-10-16 19:05:58 +0300

    ex_39.py

--- ex_39.py
+++ ex_39.py
@@ -1,11 +1,13 @@
 month = input()
 res = 0
-if month == 'Январь' or month == 'Март' or month == 'Май' or month == 'Июль' or month == 'Август' or month == 'Октябрь' or month == 'Декабрь':
+m1 = ['Январь','Март','Май', 'Июль', 'Август','Октябрь','Декабрь']
+m2 = ['Апрель', 'Июнь', 'Сентябрь', 'Ноябрь']
+if month in m1:
     res = 31
-elif month == 'Апрель' or month == 'Июнь' or month == 'Сентябрь' or month == 'Ноябрь':
+elif month in m2:
     res = 30
 else:
     res = '28 или 29'
 print(f'{month} = {res}')
 
-# сделай через списки, чтобы эти длинные строки (3 и 5) исчезли 
+
