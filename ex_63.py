e3b63aac6c60a8a36453ab790abb6e095651c3ed
Юлиана <gorbacheva.juliana@gmail.com>
2024-12-26 09:24:16 +0300

    ex_63.py

--- ex_63.py
+++ ex_63.py
@@ -2,15 +2,17 @@ numbers = int(input())
 list = []
 if numbers == 0: 
     print('Ошибка')
+    exit()
+else:
+    list.append(numbers)
 while numbers != 0:
     numbers = int(input())
     list.append(numbers)
 if 0 in list:
+    print('Ошибка')
     list.remove(0)
 print(list)
 print(sum(list))
 print(len(list))
 print(f'Среднее значение: {sum(list) / len(list)}')
 
-# Если первым числом будет 0 - то будет ошибка ZeroDivisionError: division by zero
-# Первое число не учитывается при подсчете среднего
